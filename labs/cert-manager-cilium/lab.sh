8e1c07f3e5c374609f702f201b3d60230fd291bd
Yutaro Hayakawa <yhayakawa3720@gmail.com>
2024-12-25 23:51:30 +0900

    Use Nix shell instead of Makefile
    Signed-off-by: Yutaro Hayakawa <yhayakawa3720@gmail.com>

--- labs/cert-manager-cilium/lab.sh
+++ labs/cert-manager-cilium/lab.sh
@@ -1,42 +1,67 @@
-CILIUM_VERSION ?= v1.16.5
-CERTMANAGER_VERSION ?= v1.16.2
+#!/usr/bin/env nix-shell
+#! nix-shell -i bash
+#! nix-shell -p bash kind cilium-cli kubectl kubernetes-helm
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/2ea61d0eb4f829f8e2e905f0be681a5138d519c9.tar.gz
 
-deploy:
-	@echo Creating Kind cluster and install Cilium
+function deploy() {
+	echo Create Kind cluster and install Cilium
 	kind create cluster --config cluster.yaml
 	cilium install \
-		--version $(CILIUM_VERSION) \
 		--set ingressController.enabled=true \
 		--set ingressController.loadbalancerMode=dedicated \
 		--set ingressController.default=true \
 		--set kubeProxyReplacement=true \
 		--set l2announcements.enabled=true
 	cilium status --wait
-	@echo Waiting for cilium-operator to install CRDs
+
+	echo Wait for cilium-operator to install CRDs
 	sleep 5
-	@echo Generating CA certificate
+
+	echo Generate CA certificate
 	openssl req -x509 -newkey ec -pkeyopt ec_paramgen_curve:P-256 -days 3650 -nodes -keyout ca.key -out ca.crt -subj "/CN=ExampleCA"
 	kubectl create secret tls ca-secret --cert=ca.crt --key=ca.key
 	rm ca.crt ca.key
-	@echo Install cert-manager
+
+	echo Install cert-manager
 	helm install cert-manager jetstack/cert-manager \
 		--namespace cert-manager \
 		--create-namespace \
-		--version $(CERTMANAGER_VERSION) \
 		--set crds.enabled=true \
 		--wait
-	@echo Applying all resources
-	kubectl apply -f resources.yaml
-	@echo Provisioning client container
-	docker run -d --name cert-manager-cilium-client --privileged --rm --net kind nicolaka/netshoot:latest sleep infinite
-	docker exec -it cert-manager-cilium-client ip route add `kubectl get ingress httpbin -o jsonpath="{.status.loadBalancer.ingress[0].ip}")`/32 dev eth0
 
-destroy:
+	echo Apply all resources
+	kubectl apply -f resources.yaml
+
+	echo Provision client container
+	lbip=$(kubectl get ingress httpbin -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
+	docker run -d --name cert-manager-cilium-client --privileged --rm --net kind nicolaka/netshoot:latest sleep infinite
+	docker exec -it cert-manager-cilium-client ip route add $lbip/32 dev eth0
+}
+
+function destroy() {
 	kind delete clusters cert-manager-cilium
 	docker kill cert-manager-cilium-client
+}
 
-test:
+function smoke_test() {
+	lbip=$(kubectl get ingress httpbin -o jsonpath="{.status.loadBalancer.ingress[0].ip}")
 	docker exec -it cert-manager-cilium-client \
-		curl -vk --resolve example.example.com:443:`kubectl get ingress httpbin -o jsonpath="{.status.loadBalancer.ingress[0].ip}")` \
+		curl -vk --resolve example.example.com:443:$lbip \
 			--retry 10 --retry-delay 1 \
 			https://example.example.com/get
+}
+
+case "$1" in
+	deploy)
+		deploy
+		;;
+	destroy)
+		destroy
+		;;
+	smoke_test)
+		smoke_test
+		;;
+	*)
+		echo "Unknown command $1"
+		;;
+esac
